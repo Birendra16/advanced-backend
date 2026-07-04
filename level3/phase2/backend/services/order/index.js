8f0beaa62404ecaa1a53faacc69c7de1483e514d
Birendra Bohara <bbirendra693@gmail.com>
2026-07-04 17:00:11 +0545

    refactor(level3): reorganize into phases and introduce microservices architecture
    - Moved the existing single-server architecture and configuration under level3 to level3/phase1/ for better project organization and versioning.
    - Introduced level3/phase2/ featuring a new microservices-based architecture.
    - Implemented an API Gateway and individual backend services (Auth, Order, Product) within phase2/backend.
    - Added a frontend client and updated NGINX configuration tailored for the phase2 environment.
    - Added a new docker-compose.yml for orchestrating the multi-container phase2 setup.

---
+++ level3/phase2/backend/services/order/index.js
@@ -0,0 +1,19 @@
+import express from "express"
+import dotenv from "dotenv"
+
+dotenv.config()
+
+const port = process.env.PORT || 5000
+
+const app = express()
+app.use(express.json())
+
+
+app.get("/", (req, res) => {
+    return res.status(200).json({ message: "Hello from order services" })
+})
+
+app.listen(port, () => {
+    console.log(`server started ${port}`)
+})
+
